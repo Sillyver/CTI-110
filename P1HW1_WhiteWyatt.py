23c6730397daaaf5c0b6e00e1a3b229ada517bdf
Sillyver <wyattwhite1012@gmail.com>
2026-09-02 14:30:27 -0400

    Add files via upload

---
+++ P1HW1_WhiteWyatt.py
@@ -0,0 +1,26 @@
+# Wyatt White
+# 9/2/2026
+# P1HW1
+# This project takes a input of numbers and processes them to display a result to the user
+
+print("----Calculating Exponents-----")
+print(" ")
+print(" ")
+base = int(input("Enter an integer as the base value: "))
+expo =  int(input("Enter an integer as the exponent: "))
+answer1 = base ** expo
+print(" ")
+print(" ")
+print(base,"to the power of",expo,"is",answer1,"!!!")
+print(" ")
+print(" ")
+print("-----Addition and Subtraction-----")
+print(" ")
+print(" ")
+base_int = int(input("Enter a starting integer: "))
+add_int = int(input("Enter an integer to add: "))
+sub_int = int(input("Enter an integer to subtract: "))
+answer2 = base_int + add_int - sub_int
+print(" ")
+print(" ")
+print(base_int,"+",add_int,"-",sub_int,"is equal to",answer2)
