c40ff51cbb6ae8cf987e91dd01901d6adb66ad3c
Sillyver <wyattwhite1012@gmail.com>
2026-09-02 13:52:10 -0400

    Add files via upload

---
+++ P1LAB1_WhiteWyatt.py
@@ -0,0 +1,9 @@
+#Wyatt White
+#9-2-26
+#P1LAB1
+#This project request your first name then your last name then prints out a hello message using the inputed names.
+
+
+name = input("Enter Your Name: ")
+l_name = input("Enter Your Last Name: ")
+print("Hello,",name,l_name, "welcome to Python!")
